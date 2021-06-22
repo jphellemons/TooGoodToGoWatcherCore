4e556eb2fc0a3207e60d5a8232ccff06b4a9afe1
JohanTheDeveloper <jpwdevries@gmail.com>
2021-06-22 21:21:40 +0200

    First part of session from disk

--- TooGoodToGoWatcherCore/Program.cs
+++ TooGoodToGoWatcherCore/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
@@ -15,14 +16,13 @@ namespace TooGoodToGoWatcherCore
         static async Task Main(string[] args)
         {
             do {
+                Console.WriteLine();
+
                 using var hc = new HttpClient();
                 hc.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 hc.DefaultRequestHeaders.Add("Accept-Language", "en-US");
 
-                var response = await hc.PostAsJsonAsync(tgtgApi + "auth/v1/loginByEmail", GetLoginRequest());
-
-                //var debugResponse = await response.Content.ReadAsStringAsync();
-                var loginResponse = await response.Content.ReadFromJsonAsync<LoginResponse>();
+                var loginResponse = await GetSession(hc);
 
                 hc.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", loginResponse.AccessToken);
 
@@ -37,21 +37,49 @@ namespace TooGoodToGoWatcherCore
 };*/
 
                 var lfResponse = await hc.PostAsJsonAsync(tgtgApi + "item/v7/", GetListFavorites(loginResponse.StartupData.User.UserId));
-                var debugResponse = await lfResponse.Content.ReadAsStringAsync();
-                var favResponse = await lfResponse.Content.ReadFromJsonAsync<FavoriteResponse>();
-               
-                foreach (var item in favResponse.Items.Where(i => i.InSalesWindow && i.ItemsAvailable > 0))
+
+                if (lfResponse.StatusCode == HttpStatusCode.OK)
                 {
-                    System.Console.WriteLine($"{item.DisplayName}, {item.Item.Price}");
-                    /*new ToastContentBuilder()
-    .AddArgument("action", "viewConversation")
-    .AddArgument("conversationId", 9813)
-    .AddText(item.DisplayName)
-    .AddText("Check this out, The Enchantments in Washington!")
-    .Show();*/
+                    var debugResponse = await lfResponse.Content.ReadAsStringAsync();
+                    var favResponse = await lfResponse.Content.ReadFromJsonAsync<FavoriteResponse>();
+
+                    foreach (var item in favResponse.Items.Where(i => i.InSalesWindow && i.ItemsAvailable > 0))
+                    {
+                        System.Console.WriteLine($"{item.DisplayName}, {item.Item.Price}");
+                        /*new ToastContentBuilder()
+                            .AddArgument("action", "viewConversation")
+                            .AddArgument("conversationId", 9813)
+                            .AddText(item.DisplayName)
+                            .AddText("Check this out, The Enchantments in Washington!")
+                            .Show();*/
+                    }
                 }
-                Console.Read();
+                else
+                {
+                    Console.WriteLine($"Loading Favorite failed with code {lfResponse.StatusCode}");
+                }
+
+                //Console.Read();
+                Console.WriteLine("Waiting for next round. Press Q to continue");
             } while (Console.ReadKey().Key.Equals(ConsoleKey.Q));
+        }
+
+        private static async Task<LoginResponse> GetSession(HttpClient httpClient)
+        {
+            TooGoodToGoSessionLoader tooGoodToGoSessionLoader = new TooGoodToGoSessionLoader();
+            var loginResponse = await tooGoodToGoSessionLoader.Load();
+
+            if (loginResponse == null || string.IsNullOrEmpty(loginResponse.RefreshToken))
+            {
+                var response = await httpClient.PostAsJsonAsync(tgtgApi + "auth/v1/loginByEmail", GetLoginRequest());
+
+                loginResponse = await response.Content.ReadFromJsonAsync<LoginResponse>();
+
+                await tooGoodToGoSessionLoader.Save(loginResponse);
+            }
+
+            return loginResponse;
+
         }
 
         private static ListFavorites GetListFavorites(long userId)
@@ -67,7 +95,8 @@ namespace TooGoodToGoWatcherCore
 
         private static LoginRequest GetLoginRequest()
         { 
-            return new LoginRequest(){
+            return new LoginRequest()
+            {
                 Email = "info@jphellemons.nl",
                 Password = "Your-password-here",
                 DeviceType = "UNKNOWN"
---
+++ TooGoodToGoWatcherCore/TooGoodToGoSessionLoader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TooGoodToGoWatcherCore
+{
+    public class TooGoodToGoSessionLoader
+    {
+        private const string tgtgSessionFile = "TooGoodToGoSession.json";
+
+        public async Task<LoginResponse> Load()
+        {
+            if (File.Exists(tgtgSessionFile))
+            {
+                using (Stream stream = File.OpenRead(tgtgSessionFile))
+                {
+                    return await JsonSerializer.DeserializeAsync<LoginResponse>(stream);
+                }
+            }
+
+            return null;
+        }
+
+        public async Task Save(LoginResponse loginResponse)
+        {
+            using (Stream stream = File.OpenWrite(tgtgSessionFile))
+            {
+                JsonSerializer.SerializeAsync(stream, loginResponse);
+            }
+        }
+    }
+}
